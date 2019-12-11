73e9cac20d74667a89d112f1de6a5b18c61961e1
ZusZus <52191105+ZusZus@users.noreply.github.com>
2019-12-11 12:59:13 +0530

    first commit

---
+++ Order_Status_Monitor/app/code/local/Gta/OrderStatusMonitor/Model/Observer.php
@@ -0,0 +1,23 @@
+<?php
+    class Gta_OrderStatusMonitor_Model_Observer
+    {
+        public function putstatefilteronorder($Observer)
+        {
+
+        	public function checkorderstatus(Varien_Event_Observer $observer)
+			{
+   				$status = $observer->getEvent()->getOrder()->getStatus();
+   				Mage::log($status(),null,'orderstatus.log');
+			}
+
+
+   			### start test observer ###
+
+            // $order = $Observer->getEvent();
+
+            // Mage::log($order->getName(),null,'orderproduct.log');
+
+            ### end test observer ###
+        }
+    }
+?>
